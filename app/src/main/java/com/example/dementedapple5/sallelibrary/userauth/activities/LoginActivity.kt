3c45f328bcb56ff0d883cbb8d39f610e3d1abf9c
JTorrus <jtorrusgalindo@gmail.com>
2018-02-14 20:34:59 +0100

    Added listeners to LoginActivity

--- app/src/main/java/com/example/dementedapple5/sallelibrary/userauth/activities/LoginActivity.kt
+++ app/src/main/java/com/example/dementedapple5/sallelibrary/userauth/activities/LoginActivity.kt
@@ -2,10 +2,12 @@ package com.example.dementedapple5.sallelibrary.userauth.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.TabLayout
 import android.view.Menu
 import android.view.MenuItem
 import com.example.dementedapple5.sallelibrary.R
 import com.example.dementedapple5.sallelibrary.userauth.adapters.SectionsPagerAdapter
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var mSectionsPageAdapter: SectionsPagerAdapter
@@ -17,6 +19,10 @@ class LoginActivity : AppCompatActivity() {
 
         mSectionsPageAdapter = SectionsPagerAdapter(supportFragmentManager)
 
+        container.adapter = mSectionsPageAdapter
+
+        container.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tablayout))
+        tablayout.addOnTabSelectedListener(TabLayout.ViewPagerOnTabSelectedListener(container))
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {

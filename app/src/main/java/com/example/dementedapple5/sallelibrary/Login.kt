60f085e5fbf1f363369f0e377d3e3ba7bdf1c7ab
dementedapple5 <daniel271196@gmail.com>
2018-02-14 18:57:03 +0100

    Initial commit

---
+++ app/src/main/java/com/example/dementedapple5/sallelibrary/Login.kt
@@ -0,0 +1,12 @@
+package com.example.dementedapple5.sallelibrary
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Login : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}

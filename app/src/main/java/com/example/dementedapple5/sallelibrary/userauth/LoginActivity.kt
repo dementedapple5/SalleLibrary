f65d32d2851290ef33111b96944187283272e32c
JTorrus <jtorrusgalindo@gmail.com>
2018-02-14 20:01:43 +0100

    Layout for login activity created, TODO functionality

--- app/src/main/java/com/example/dementedapple5/sallelibrary/userauth/LoginActivity.kt
+++ app/src/main/java/com/example/dementedapple5/sallelibrary/userauth/LoginActivity.kt
@@ -5,14 +5,18 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import com.example.dementedapple5.sallelibrary.R
+import com.example.dementedapple5.sallelibrary.userauth.adapters.SectionsPagerAdapter
 
 class LoginActivity : AppCompatActivity() {
-    private var mSectionsPageAdapter: MainActivity.SectionsPagerAdapter? = null
+    private lateinit var mSectionsPageAdapter: SectionsPagerAdapter
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        mSectionsPageAdapter = SectionsPagerAdapter(supportFragmentManager)
+        
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {

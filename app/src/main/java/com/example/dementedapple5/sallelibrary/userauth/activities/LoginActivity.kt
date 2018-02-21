62f44b217bf7373d7efebc9a3968378bbcb2dddc
JTorrus <jtorrusgalindo@gmail.com>
2018-02-21 19:24:58 +0100

    Sign in with Google button implemented in the layout, functionality to do

--- app/src/main/java/com/example/dementedapple5/sallelibrary/userauth/activities/LoginActivity.kt
+++ app/src/main/java/com/example/dementedapple5/sallelibrary/userauth/activities/LoginActivity.kt
@@ -5,24 +5,30 @@ import android.os.Bundle
 import android.support.design.widget.TabLayout
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import com.example.dementedapple5.sallelibrary.R
 import com.example.dementedapple5.sallelibrary.userauth.adapters.SectionsPagerAdapter
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import kotlinx.android.synthetic.main.activity_login.*
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var mSectionsPageAdapter: SectionsPagerAdapter
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        setSupportActionBar(toolbar)
 
-        mSectionsPageAdapter = SectionsPagerAdapter(supportFragmentManager)
+        val gso: GoogleSignInOptions = GoogleSignInOptions.DEFAULT_SIGN_IN
+        var mGoogleSignInClient: GoogleSignInClient = GoogleSignIn.getClient(this, gso)
 
-        container.adapter = mSectionsPageAdapter
-
-        container.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tablayout))
-        tablayout.addOnTabSelectedListener(TabLayout.ViewPagerOnTabSelectedListener(container))
+        signup_button.setOnClickListener(this)
     }
+
+    override fun onClick(view: View?) {
+
+    }
+
+
 }

b0c6b7dbfa50644f14a92883726f082fa6c77be5
JTorrus <jtorrusgalindo@gmail.com>
2018-03-10 18:05:10 +0100

    Some files moved and renamed

--- app/src/main/java/com/example/dementedapple5/sallelibrary/splashscreen/activities/SplashScreenActivity.kt
+++ app/src/main/java/com/example/dementedapple5/sallelibrary/splashscreen/activities/SplashScreenActivity.kt
@@ -1,9 +1,8 @@
-package com.example.dementedapple5.sallelibrary.splashscreen
+package com.example.dementedapple5.sallelibrary.splashscreen.activities
 
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.example.dementedapple5.sallelibrary.R
 import com.example.dementedapple5.sallelibrary.userauth.activities.LoginActivity
 
 class SplashScreenActivity : AppCompatActivity() {
--- app/src/main/java/com/example/dementedapple5/sallelibrary/userauth/activities/LoginActivity.kt
+++ app/src/main/java/com/example/dementedapple5/sallelibrary/userauth/activities/LoginActivity.kt
@@ -3,13 +3,9 @@ package com.example.dementedapple5.sallelibrary.userauth.activities
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.design.widget.TabLayout
-import android.view.Menu
-import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import com.example.dementedapple5.sallelibrary.R
-import com.example.dementedapple5.sallelibrary.mainmenu.MainMenuActivity
 import com.example.dementedapple5.sallelibrary.mainmenu.TabbedActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount

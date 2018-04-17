8bb527ea92e7636b1fceab7a48cd9bdc6a934b65
JTorrus <jtorrusgalindo@gmail.com>
2018-04-18 00:15:14 +0200

    Dokka implemented for Book

--- app/src/main/java/com/example/dementedapple5/sallelibrary/model/Book.kt
+++ app/src/main/java/com/example/dementedapple5/sallelibrary/model/Book.kt
@@ -2,6 +2,22 @@ package com.example.dementedapple5.sallelibrary.model
 
 import java.io.Serializable
 
+/**
+ * Representación de un libro.
+ *
+ * Esta clase consiste en definir un libro que serán los principales objetos que se mostrarán en la pantalla principal de la aplicación.
+ *
+ * @constructor Crea una instancia vacía de [Book].
+ *
+ * @property title Título.
+ * @property author Autor.
+ * @property releaseDate Fecha de salida.
+ * @property publisher Editorial.
+ * @property genre Género.
+ * @property numPages Número de páginas.
+ * @property description Descripción.
+ * @property img Imagen de portada.
+ */
 class Book(): Serializable {
     var title: String = ""
     var publisher: String = ""
@@ -12,6 +28,9 @@ class Book(): Serializable {
     var releaseDate: String = ""
     var description: String = ""
 
+    /**
+     * @constructor Crea una instancia de [Book] con una serie de propiedades.
+     */
     constructor(title: String, author: String, releaseDate: String, publisher: String, genre: String, numPages: Int, description: String, img: String) : this() {
         this.title = title
         this.img = img
@@ -23,6 +42,9 @@ class Book(): Serializable {
         this.description = description
     }
 
+    /**
+     * Permite imprimir un [String] conteniendo información sobre el [title] y [author].
+     */
     override fun toString(): String {
         return "Book(title='$title', author='$author')"
     }
